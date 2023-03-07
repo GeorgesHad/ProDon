f608629e43d2cc617d39ed96bb922d7e144ff0de
Georges <geomia2@hotmail.com>
2023-03-07 02:36:27 +0200

    Java assigning elements from xml

--- app/src/main/java/com/example/prodon/ui/add/AddFragment.java
+++ app/src/main/java/com/example/prodon/ui/add/AddFragment.java
@@ -4,35 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.prodon.databinding.FragmentAddBinding;
+import com.example.prodon.R;
 
 
 public class AddFragment extends Fragment {
-
-    private FragmentAddBinding binding;
-
+    private Button submitBtn;
+    private EditText fName,lName,parentName,parentPhone,playerPhone,year;
+    private View v;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        AddViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(AddViewModel.class);
+        v =inflater.inflate(R.layout.fragment_add, container, false);
+        fName = v.findViewById(R.id.FName);
+        lName = v.findViewById(R.id.LName);
+        parentName  = v.findViewById(R.id.parentName);
+        parentPhone  = v.findViewById(R.id.parentPhone);
+        playerPhone = v.findViewById(R.id.playerPhone);
+        year = v.findViewById(R.id.year);
+        submitBtn = v.findViewById(R.id.button);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        binding = FragmentAddBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+            }
+        });
+        return v;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
---
+++ app/src/main/java/com/example/prodon/ui/find/FindFragment.java
@@ -0,0 +1,49 @@
+package com.example.prodon.ui.find;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.prodon.R;
+import com.example.prodon.databinding.FragmentFindBinding;
+
+
+public class FindFragment extends Fragment {
+    private View v;
+    private EditText fName,lName;
+    private TextView txtEmpty;
+    private Button searchBtn;
+    private RecyclerView rec;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_find,container,false);
+        fName = v.findViewById(R.id.searchFName);
+        lName = v.findViewById(R.id.searchLName);
+        txtEmpty = v.findViewById(R.id.txtEmpty);
+        searchBtn = v.findViewById(R.id.button2);
+        rec = v.findViewById(R.id.recycler);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+    }
+}
