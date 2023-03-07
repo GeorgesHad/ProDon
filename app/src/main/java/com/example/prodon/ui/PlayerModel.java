5d918a38a763364f296f89493e004aa0f0a5862a
Georges <geomia2@hotmail.com>
2023-03-07 13:06:55 +0200

    Model of data added

---
+++ app/src/main/java/com/example/prodon/ui/PlayerModel.java
@@ -0,0 +1,65 @@
+package com.example.prodon.ui;
+
+import androidx.annotation.Nullable;
+
+public class PlayerModel {
+    private String fName,lName,parentName,parentPhone,playerPhone;
+    private int year;
+
+    public PlayerModel(String fName, String lName, String parentName, @Nullable String parentPhone,@Nullable String playerPhone, int year) {
+        this.fName = fName;
+        this.lName = lName;
+        this.parentName = parentName;
+        this.parentPhone = parentPhone;
+        this.playerPhone = playerPhone;
+        this.year = year;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
+    public String getParentPhone() {
+        return parentPhone;
+    }
+
+    public void setParentPhone(String parentPhone) {
+        this.parentPhone = parentPhone;
+    }
+
+    public String getPlayerPhone() {
+        return playerPhone;
+    }
+
+    public void setPlayerPhone(String playerPhone) {
+        this.playerPhone = playerPhone;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
