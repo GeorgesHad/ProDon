b282b1de1d97bf2db72a592d24039d87efa401ea
Georges <geomia2@hotmail.com>
2023-04-29 23:09:02 +0300

    made the app open whatsapp to send digital receipt after making a payment

--- app/src/main/java/com/example/prodon/ui/sqliteHelper/PlayersRecyclerAdapter.java
+++ app/src/main/java/com/example/prodon/ui/sqliteHelper/PlayersRecyclerAdapter.java
@@ -1,11 +1,18 @@
 package com.example.prodon.ui.sqliteHelper;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.TextView;
@@ -19,7 +26,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.prodon.R;
 import com.example.prodon.ui.find.PlayerDetails;
 
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PlayersRecyclerAdapter extends RecyclerView.Adapter<PlayersRecyclerAdapter.ViewHolder> {
 
@@ -59,9 +68,15 @@ public class PlayersRecyclerAdapter extends RecyclerView.Adapter<PlayersRecycler
         holder.txtYear.setText(c);
         holder.txtName.setText(a);
         int s = players.get(position).getId();
+        holder.statusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+           }
+            });
         holder.payBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
                 builder.setTitle("Details about the drive");
 
@@ -74,23 +89,76 @@ public class PlayersRecyclerAdapter extends RecyclerView.Adapter<PlayersRecycler
                 NumberPicker numberPicker = customLayout.findViewById(R.id.numberPicker);
                 EditText value = customLayout.findViewById(R.id.editValue);
                 EditText year = customLayout.findViewById(R.id.editYear);
+                CheckBox parentCheck = customLayout.findViewById(R.id.parentCheck);
+                CheckBox playerCheck= customLayout.findViewById(R.id.playerCheck);
+                EditText phone = customLayout.findViewById(R.id.otherPhone);
+                if (players.get(holder.getAdapterPosition()).getPlayerPhone() != null)
+                {
+                    playerCheck.setText(players.get(holder.getAdapterPosition()).getPlayerPhone());
+                }
+                else
+                {
+                    playerCheck.setClickable(false);
+                    playerCheck.setText("No number.");
+                }
+                if (players.get(holder.getAdapterPosition()).getParentPhone() != null)
+                {
+                    parentCheck.setText(players.get(holder.getAdapterPosition()).getParentPhone());
+                }
+                else
+                {
+                    playerCheck.setClickable(false);
+                    playerCheck.setText("No number.");
+                }
+                parentCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        playerCheck.setChecked(false);
+                    }
+                });
+                playerCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        parentCheck.setChecked(false);
+                    }
+                });
+                numberPicker.setMinValue(1);
+                numberPicker.setMaxValue(12);
                 closeBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         dialog.dismiss();
                     }
                 });
+
+
+
                 submitBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        int val = Integer.parseInt(value.getText().toString());
-                        int y =  Integer.parseInt(year.getText().toString());
-                        int m = numberPicker.getValue();
+                        int val,y,m;
+                       if (!value.getText().toString().equals("") && !year.getText().toString().equals("")){ val = Integer.parseInt(value.getText().toString());  y =  Integer.parseInt(year.getText().toString());m = numberPicker.getValue();}
+                       else {Toast.makeText(i.getBaseContext(), "Please input all required values.", Toast.LENGTH_SHORT).show(); return;}
                     db.addNewPayment(s,y,m,val);
+                        String mobileNumber;
+                        if (parentCheck.isChecked()){mobileNumber = parentCheck.getText().toString();}
+                        else if (playerCheck.isChecked()){mobileNumber=playerCheck.getText().toString();}
+                        else if(phone.getText()!=null) mobileNumber = phone.getText().toString();
+                        else {Toast.makeText(i.getBaseContext(), "Please select a phone number.", Toast.LENGTH_SHORT).show(); return;}
+
+                        String message = "Player Name: "+players.get(holder.getAdapterPosition()).getfName()+" "+players.get(holder.getAdapterPosition()).getlName()  +"\nPayment made for: "+m+"-"+y + "Value: "+val;
+                        boolean installed = true;//appInstalledOrNot("com.whatsapp");
+                        if (installed) {
+                            Intent intent = new Intent(Intent.ACTION_VIEW);
+                            intent.setData(Uri.parse("http://api.whatsapp.com/send?phone=" +  mobileNumber + "&text=" + message));
+                            i.startActivity(intent);
+                        } else {
+                            Toast.makeText(i.getBaseContext(), "Whats app not installed on your device", Toast.LENGTH_SHORT).show();
+                        }
+                        Toast.makeText(i.getBaseContext(), "Payment made successfully.", Toast.LENGTH_SHORT).show();
                     }
                 });
-                numberPicker.setMinValue(1);
-                numberPicker.setMaxValue(12);
+
                 dialog.show();
 
             }
@@ -136,6 +204,17 @@ public class PlayersRecyclerAdapter extends RecyclerView.Adapter<PlayersRecycler
             v= itemView.findViewById(R.id.parent);
 
         }
+    }
+    private boolean appInstalledOrNot(String url){
+        PackageManager packageManager =i.getPackageManager();
+        boolean app_installed;
+        try {
+            packageManager.getPackageInfo(url,PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        }catch (PackageManager.NameNotFoundException e){
+            app_installed = false;
+        }
+        return app_installed;
     }
 }
 
